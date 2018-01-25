bf6786b370cb7b1275fd6c8e487906d84e642cc5
David Do <daviddo@davids-mbp.uphs.upenn.edu>
2018-01-25 14:06:52 -0500

    graphs tweets per hour

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,21 @@
 class Tweet < ApplicationRecord
   belongs_to :channel
+  
+  def parsed_json
+    @parsed_json ||= JSON.parse(self.full_json)
+  end
+   
+  def unique_tweets_per_hour
+    1.0 * n_unique_tweets / time_span
+  end
+  
+  def n_unique_tweets
+    parsed_json.uniq {|e| e["text"].gsub(/\d/, "0")}.size  
+  end
+  
+  def time_span
+    earliest_at = Time.parse( parsed_json.last["created_at"] )
+    latest_at = Time.parse( parsed_json.first["created_at"] )
+    (latest_at - earliest_at) / 1.hour
+  end
 end
