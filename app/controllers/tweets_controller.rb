5835fcf38cae13d8f4ee064df62113bd9f5b19c3
David Do <daviddo@Davids-MacBook-Pro.local>
2018-01-24 22:25:02 -0500

    saves tweet results

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,26 @@ class TweetsController < ApplicationController
   def index
     
     if params[:fetch] == "t"
-      raise "will try to fetch"
+
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+      end
+      
+      tweets = client.search("XRP", result_type: "recent", count: 100).collect do |tweet|
+        # puts tweet.favorite_count
+        # puts tweet.retweet_count
+        # puts tweet.full_text
+        # puts tweet    e.g.  <Twitter::Tweet:0x007f9ef54c4f78>
+        # puts tweet.to_json # {...}
+
+
+        tweet
+      end
+      
+      Tweet.create( keyword: "XRP", unique_id:  nil, full_json: tweets.to_json)
     end
     
     @tweets = Tweet.all
