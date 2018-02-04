614261099c034b17956d676540df00d19f904ec1
David Do <daviddo@Davids-MacBook-Pro.local>
2018-02-03 23:05:33 -0500

    raise delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # to enable gmail to send
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
